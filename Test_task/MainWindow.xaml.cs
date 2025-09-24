e70a12da5ab0855748164529c1b7b33ea0b6aed7
Stefan272703 <stepan.petrovskiy@yandex.ru>
2025-09-24 23:07:09 +0300

    Реализация логики главного окна

--- Test_task/MainWindow.xaml.cs
+++ Test_task/MainWindow.xaml.cs
@@ -32,17 +32,17 @@ namespace Test_Task
     /// </summary>
     public partial class MainWindow : Window
     {
-        public ObservableCollection<File_JSON> files = new ObservableCollection<File_JSON>();
+        public ObservableCollection<FileJson> Files = new ObservableCollection<FileJson>();
         public MainWindow()
         {
             InitializeComponent();
-            FileData.ItemsSource = files;
+            FileData.ItemsSource = Files;
         }
 
 
 
         // Функция добавления файла в список
-        private void Add_File_Click(object sender, RoutedEventArgs e)
+        private void AddFile_Click(object sender, RoutedEventArgs e)
         {
 
             OpenFileDialog openFileDialog = new OpenFileDialog();
@@ -56,27 +56,24 @@ namespace Test_Task
                 fileinfo = new FileInfo(filepath);
 
 
-                if (files.Select(x => x.FilePath).Contains(filepath)) // если список путей к файлу содержит выьранный пользователем путь к файлу 
+                if (Files.Select(x => x.FilePath).Contains(filepath)) // если список путей к файлу содержит выьранный пользователем путь к файлу 
                 {
-                    //var msgbox = MessageBox.Show("Выбранный файл со схожими путем и именем уже есть в файле-списке, все равно добавить файл в список?", "Одинаковый путь и имя файла", MessageBoxButton.YesNo, MessageBoxImage.Question);
-
                     byte[] fileBytes = File.ReadAllBytes(filepath); // считываем байты файла
 
                     uint crc32 = CRC32.CalculateCRC32(fileBytes); // рассчитываем CRC32 файла
 
-
-                    foreach (var file in files)
+                    foreach (var file in Files)
                     {
                         if (file.Checksum != $"{crc32:X8}" && file.FilePath == filepath)
                         {
-                            var msg = MessageBox.Show("Выбранный файл со схожим путем и именем есть в файле-списе, но отличаются контрольной суммой. Обновить контрольную сумму существующего файла?",
+                            var message = MessageBox.Show("Выбранный файл со схожим путем и именем есть в файле-списе, но отличаются контрольной суммой. Обновить контрольную сумму существующего файла?",
                                 "Одинаковый путь и имя файла",
                                 MessageBoxButton.YesNo,
                                 MessageBoxImage.Question);
-                            switch (msg)
+                            switch (message)
                             {
                                 case MessageBoxResult.Yes:
-                                    files[files.IndexOf(file)].Checksum = $"{crc32:X8}";
+                                    Files[Files.IndexOf(file)].Checksum = $"{crc32:X8}";
                                     FileData.Items.Refresh(); // Обновляем таблицу, для отображения новго результата
                                     break;
                                 case MessageBoxResult.No:
@@ -86,24 +83,10 @@ namespace Test_Task
                         }
                     }
 
-                    var msgbox = MessageBox.Show("Выбранный файл со схожими путем и именем уже есть в файле-списке, как и контрольная сумма", "Одинаковый путь и имя файла", MessageBoxButton.OK, MessageBoxImage.Information);
-                    /*switch (msgbox) // Выбор добавить файл(да/нет)
-                    {
-                        case MessageBoxResult.OK:
-                            break;
-                        case MessageBoxResult.Yes: // В случае да
-                            {
-                                case MessageBoxResult.Yes: // Если да, то изменяем у существующего файла индекс
-                                    files[files.IndexOf(file)].Checksum = $"{crc32:X8}"; // находим индекс, по которому поменяем значение контрольной суммы
-                                    FileData.Items.Refresh(); // обновляем таблицу файла-списка
-
-                                    break;
-                                case MessageBoxResult.No: // Если нет, то ничего не изменяем и не добавляем
-                                    break;
-                            }
-                        case MessageBoxResult.No: // в случае нет
-                            return;
-                    }*/
+                    var msgbox = MessageBox.Show("Выбранный файл со схожими путем и именем уже есть в файле-списке, как и контрольная сумма",
+                        "Одинаковый путь и имя файла",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Information);
                 }
                 else // иначе если список путей к файлу не содержит путь к файлу, выбранный пользователем, то просто добавляем файл в список
                 {
@@ -119,11 +102,11 @@ namespace Test_Task
                 uint crc32 = CRC32.CalculateCRC32(fileBytes); // рассчет контрольной суммы файла под CRC32
 
                 
-                var check = Check_dublicates_Checksum($"{crc32:X8}"); // Проверка на совпадение контрольной суммы
+                var check = CheckDublicatesChecksum($"{crc32:X8}"); // Проверка на совпадение контрольной суммы
 
                 if (check.Item1) // Если есть дубликат и пользователь выбрал поменять контрольную сумму
                 {
-                    while (files.Select(x => x.Checksum).Contains($"{crc32:X8}")) // пока контрольная сумма добавляемого файла не станет уникальной
+                    while (Files.Select(x => x.Checksum).Contains($"{crc32:X8}")) // пока контрольная сумма добавляемого файла не станет уникальной
                     {
                         using (var stream = new FileStream(filepath, FileMode.Append))
                         {
@@ -134,17 +117,17 @@ namespace Test_Task
 
                         crc32 = CRC32.CalculateCRC32(fileBytes); // рассчет контрольной суммы файла под CRC32
                     }
-                    var file = new File_JSON { File_Name = fileinfo.Name, Checksum = $"{crc32:X8}", FilePath = filepath };
+                    var file = new FileJson { File_Name = fileinfo.Name, Checksum = $"{crc32:X8}", FilePath = filepath };
 
-                    files.Add(file); // Добавление в список информации о файле
+                    Files.Add(file); // Добавление в список информации о файле
                 }
                 else // иначе если  нет дубликата или пользователь отказался менять контрольную сумму
                 {
                     crc32 = CRC32.CalculateCRC32(fileBytes); // рассчет контрольной суммы файла под CRC32
 
-                    var file = new File_JSON { File_Name = fileinfo.Name, Checksum = $"{crc32:X8}", FilePath = filepath };
+                    var file = new FileJson { File_Name = fileinfo.Name, Checksum = $"{crc32:X8}", FilePath = filepath };
 
-                    files.Add(file);
+                    Files.Add(file);
                 }
             }
         }
@@ -152,54 +135,37 @@ namespace Test_Task
 
 
         //Сохранение файла
-        private void Save_File(object sender, RoutedEventArgs e)
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
         {
-            File_JSON.Save_Json(files); // вызов метода сохранения файла в формат JSON
+            FileJson.SaveJson(Files); // вызов метода сохранения файла в формат JSON
 
         }
 
         // Импорт файла
-        private void Import_File(object sender, RoutedEventArgs e)
+        private void ImportFile(object sender, RoutedEventArgs e)
         {
-            var files_json = File_JSON.Import_JSON(); // получаем список файлов, сохраненных в JSON
+            var files_json = FileJson.Import_JSON(); // получаем список файлов, сохраненных в JSON
             if (files_json == null) { }
             else
             {
-                //files.CollectionChanged += File_JSON_CollectionChanged; // Подписка на событие, если происходит добавление файла в список
                 foreach (var file in files_json) // Импорт файла из сохраненного файла-списка JSON
                 {
                     // Добавляем каждый файл в список
-                    Check_Has_Or_Correct_CheckSum(file); // вызов метода для наличия или проверки на корректность контрольной суммы
+                    CheckHasOrCorrectCheckSum(file); // вызов метода для наличия или проверки на корректность контрольной суммы
 
                 }
                 MessageBox.Show("Файл импортирован в таблицу");
-
-                //files.CollectionChanged -= File_JSON_CollectionChanged; // Отписка на соыбтие, когда закончили добавлять файлы
             }
         }
 
-        // Вызов события при любых изменениях с коллекцией (файлом-списком)
-        /*private void File_JSON_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            switch (e.Action)
-            {
-                case NotifyCollectionChangedAction.Add:
-                    break;
-                case NotifyCollectionChangedAction.Remove:
-                    break;
-                case NotifyCollectionChangedAction.Replace:
-                    break;
-            }
-        }*/
-
         //функция проверки наличия или соответветстия контрольной суммы
-        private void Check_Has_Or_Correct_CheckSum(File_JSON json_file)
+        private void CheckHasOrCorrectCheckSum(FileJson jsonfile)
         {
-            bool file_exists = File.Exists(json_file.FilePath);
+            bool file_exists = File.Exists(jsonfile.FilePath);
 
-            if (json_file.FilePath == string.Empty || !file_exists) // если путь к файлу пустой или неккоректен
+            if (jsonfile.FilePath == string.Empty || !file_exists) // если путь к файлу пустой или неккоректен
             {
-                var msg = MessageBox.Show($"отсутствует или неправильно введен путь к файлу {json_file.FilePath}. Добавить/изменить его и вставить в файл-список?", "Некоректный путь к файлу", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                var msg = MessageBox.Show($"отсутствует или неправильно введен путь к файлу {jsonfile.FilePath}. Добавить/изменить его и вставить в файл-список?", "Некоректный путь к файлу", MessageBoxButton.YesNo, MessageBoxImage.Question);
 
                 switch (msg) // Выбор добавить/изменить файл или не выбрать
                 {
@@ -211,16 +177,16 @@ namespace Test_Task
 
                         if (openFileDialog.ShowDialog() == true)
                         {
-                            string filepath = openFileDialog.FileName;
-                            var fileinfo = new FileInfo(filepath);
+                            string filePath = openFileDialog.FileName;
+                            var fileinfo = new FileInfo(filePath);
 
-                            byte[] fileBytes = File.ReadAllBytes(filepath); // считываем байты файла
+                            byte[] fileBytes = File.ReadAllBytes(filePath); // считываем байты файла
 
                             uint crc32 = CRC32.CalculateCRC32(fileBytes); // рассчет контрольной суммы файла под CRC32
 
-                            var file = new File_JSON { File_Name = fileinfo.Name, Checksum = $"{crc32:X8}", FilePath = filepath };
+                            var file = new FileJson { File_Name = fileinfo.Name, Checksum = $"{crc32:X8}", FilePath = filePath };
 
-                            files.Add(file);
+                            Files.Add(file);
 
                         }
                         else
@@ -236,24 +202,24 @@ namespace Test_Task
             }
             else
             {
-                var fileinfo = new FileInfo(json_file.FilePath); // информация о файле
+                var fileinfo = new FileInfo(jsonfile.FilePath); // информация о файле
 
-                byte[] fileBytes = File.ReadAllBytes(json_file.FilePath); // считываем байты файла
+                byte[] fileBytes = File.ReadAllBytes(jsonfile.FilePath); // считываем байты файла
 
                 uint crc32 = CRC32.CalculateCRC32(fileBytes); // рассчет контрольной суммы файла под CRC32
 
-                if(json_file.Checksum != $"{crc32:X8}") // Если неккореткно рассчитана контрольная сумма
+                if(jsonfile.Checksum != $"{crc32:X8}") // Если неккореткно рассчитана контрольная сумма
                 {
-                   var msg = MessageBox.Show($"для данного файла {json_file.FilePath} отсутствует или неккоректно рассчитана контрольная сумма. Пересчитать и вставить файл в список?",
+                   var message = MessageBox.Show($"для данного файла {jsonfile.FilePath} отсутствует или неккоректно рассчитана контрольная сумма. Пересчитать и вставить файл в список?",
                        "Контрольная сумма",
                        MessageBoxButton.YesNo,
                        MessageBoxImage.Question);
 
-                    switch (msg) // Выбор рассчитать контрольную сумму и вставить файл или не добавлять
+                    switch (message) // Выбор рассчитать контрольную сумму и вставить файл или не добавлять
                     {
                         case MessageBoxResult.Yes: // Добавляе файл
-                            var file = new File_JSON { File_Name = fileinfo.Name, Checksum = $"{crc32:X8}", FilePath = json_file.FilePath };
-                            files.Add(file);
+                            var file = new FileJson { File_Name = fileinfo.Name, Checksum = $"{crc32:X8}", FilePath = jsonfile.FilePath };
+                            Files.Add(file);
                             break;
                         case MessageBoxResult.No: // Не добавляем файл
                             break;
@@ -261,18 +227,18 @@ namespace Test_Task
                 }
                 else // Иначе, если контролльная сумма рассчитана правильно
                 {
-                    var file = new File_JSON { File_Name = fileinfo.Name, Checksum = $"{crc32:X8}", FilePath = json_file.FilePath };
-                    files.Add(file);
+                    var file = new FileJson { File_Name = fileinfo.Name, Checksum = $"{crc32:X8}", FilePath = jsonfile.FilePath };
+                    Files.Add(file);
                 }
 
             }
         }
 
         // функция проверки на дубликаты контрольные суммы
-        (bool, int) Check_dublicates_Checksum(string cur_checksum)
+        (bool, int) CheckDublicatesChecksum(string cur_checksum)
         {
-            files.Select(x => x.Checksum); // Выбираем только контрольные суммы
-            if (files.Select(x => x.Checksum).Contains(cur_checksum)) // если файл-список содержит уже контрольную сумму
+            Files.Select(x => x.Checksum); // Выбираем только контрольные суммы
+            if (Files.Select(x => x.Checksum).Contains(cur_checksum)) // если файл-список содержит уже контрольную сумму
             {
                 var msg = MessageBox.Show("Контрольная сумма файла уже существует в файле-списке\n Изменить ее и добавить файл (Да)\n Просто добавить файл без изменений (Нет)?",
                     "Совпадение контрольных сумм",
@@ -281,9 +247,8 @@ namespace Test_Task
                 switch (msg) // Выбор изменять не изменять контрольную сумму
                 {
                     case MessageBoxResult.Yes: // Возвращаем истину
-                        return (true, files.Select(x => x.Checksum).Where(x => x == cur_checksum).Count()); // вовзращаем истину и количество изменений
+                        return (true, Files.Select(x => x.Checksum).Where(x => x == cur_checksum).Count()); // вовзращаем истину и количество изменений
                     case MessageBoxResult.No: // возвращаем ложь
-                        //MessageBox.Show("Добавлен файл без изменения контрольной суммы", "Добавление", MessageBoxButton.OK, MessageBoxImage.Information);
                         return (false, 0);
                 }
             }
@@ -295,47 +260,45 @@ namespace Test_Task
         {
             if(e.EditAction == DataGridEditAction.Commit) //Действие подтверждено(изменение ячейки произошло)
             {
-                //files.CollectionChanged += File_JSON_CollectionChanged;
                 if (e.Column is DataGridBoundColumn column)
                 {
                     var bindingPath = (column.Binding as Binding).Path.Path;
 
                     if (bindingPath == "File_Name")
                     {
-                        Commit_Change();
+                        CommitChange();
                     }
                     else if (bindingPath == "Checksum")
                     {
-                        Commit_Change();
+                        CommitChange();
                     }
                     else if (bindingPath == "FilePath")
                     {
-                        Commit_Change();
+                        CommitChange();
                     }
                 }
-                //files.CollectionChanged -= File_JSON_CollectionChanged;
             }
-            void Commit_Change()
+            void CommitChange()
             {
                 int rowIndex = e.Row.GetIndex();
                 var textbox = e.EditingElement as TextBox;
 
                 var text = textbox.Text;
 
-                if (rowIndex >= 0 && rowIndex < files.Count)
+                if (rowIndex >= 0 && rowIndex < Files.Count)
                 {
-                    var item = files[rowIndex];
+                    var item = Files[rowIndex];
 
                     item.FilePath = text;
-                    files[rowIndex] = item;
+                    Files[rowIndex] = item;
                 }
             }
         }
 
-        // Функция удаления строки/строк при нажатии на кнопку "Удалить "
-        private void Delete_Row(object sender, RoutedEventArgs e)
+        // Функция удаления строки/строк при нажатии на кнопку "Удалить"
+        private void DeleteRow(object sender, RoutedEventArgs e)
         {
-            var selectedFiles = FileData.SelectedItems.Cast<File_JSON>().ToList();
+            var selectedFiles = FileData.SelectedItems.Cast<FileJson>().ToList();
 
             if(selectedFiles.Count == 0)
             {
@@ -344,15 +307,13 @@ namespace Test_Task
                 return;
             }
 
-            //files.CollectionChanged += File_JSON_CollectionChanged;
             foreach (var file in selectedFiles) // выбираем каждую выделенную строку для удаления файла из списка
             {
-                if (files.Contains(file))
+                if (Files.Contains(file))
                 {
-                    files.Remove(file);
+                    Files.Remove(file);
                 }
             }
-            //files.CollectionChanged -= File_JSON_CollectionChanged;
         }
     }
 }
